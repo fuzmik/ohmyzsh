e6ca8dd58f04f5d34f07c4cfe1e25d731a54fc95
frank <sljbbr@gmail.com>
2025-01-11 14:41:00 -0800

    update

--- zsh-custom-files/ssh-agent.zsh
+++ zsh-custom-files/ssh-agent.zsh
@@ -1,5 +1,5 @@
-zstyle :omz:plugins:ssh-agent quiet yes
-zstyle :omz:plugins:ssh-agent lazy yes
+#zstyle :omz:plugins:ssh-agent quiet yes
+#zstyle :omz:plugins:ssh-agent lazy yes
 zstyle :omz:plugins:ssh-agent agent-forwarding yes
 zstyle :omz:plugins:ssh-agent lifetime 24h
 zstyle :omz:plugins:ssh-agent identities id_rsa eos.lan id_ed25519
