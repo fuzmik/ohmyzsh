eaab2cf15bcfc47384eca3cbaf4b7fbe6c0d95bf
frank <sljbbr@gmail.com>
2025-01-17 18:22:53 -0800

    update

--- zsh-custom-files/ssh-agent.zsh
+++ zsh-custom-files/ssh-agent.zsh
@@ -3,4 +3,4 @@
 zstyle :omz:plugins:ssh-agent agent-forwarding yes
 zstyle :omz:plugins:ssh-agent lifetime 24h
 zstyle :omz:plugins:ssh-agent identities id_rsa eos.lan id_ed25519
-zstyle :omz:plugins:ssh-agent ssh-add-args -K -c -a $XDG_RUNTIME_DIR/ssh-agent.socket
+#zstyle :omz:plugins:ssh-agent ssh-add-args -K -c $XDG_RUNTIME_DIR/ssh-agent.socket
