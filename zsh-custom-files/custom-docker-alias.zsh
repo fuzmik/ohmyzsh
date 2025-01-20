a644f9f58efbb6b9de282a404b40723bceee010f
frank <sljbbr@gmail.com>
2025-01-19 17:14:46 -0800

    update

--- zsh-custom-files/custom-docker-alias.zsh
+++ zsh-custom-files/custom-docker-alias.zsh
@@ -14,9 +14,8 @@ function dc-autocompose {
     docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ghcr.io/red5d/docker-autocompose "$container_id"
 }
 
-# custom functions runlike 
-#custom function dc-autocompose
-function runlike {
+# custom functions runlike
+function dc-runlike {
     # Use docker ps to list running containers, pipe the output to fzf for interactive selection
     container_id=$(docker ps --format "{{.Names}}" | fzf)
 
